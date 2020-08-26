84ee6efe009e3b4511ad14bf72ef69a240e397a5
anycad <anycad@126.com>
2020-08-26 09:08:17 +0800

    add surface

---
+++ AnyCAD.WinForms/Analysis/Analysis_Surface.cs
@@ -0,0 +1,35 @@
+﻿using AnyCAD.Forms;
+using AnyCAD.Foundation;
+using System.Windows.Forms;
+
+namespace AnyCAD.Demo.Geometry
+{
+    class Analysis_Surface : TestCase
+    {
+        public override void Run(RenderControl renderer)
+        {
+            string fileName = GetResourcePath("Holes.stp");
+            var shape = StepIO.Open(fileName);
+            if (shape == null)
+                return;
+
+            var edge = shape.FindChild(EnumTopoShapeType.Topo_EDGE, 436);
+            var face = shape.FindChild(EnumTopoShapeType.Topo_FACE, 148);
+
+            var curve = new ParametricCurve(edge);
+            curve.GetEndPoint();
+            var surface = new ParametricSurface(face);
+
+            var extrema = new ExtremaCurveSurface();
+            extrema.SetSurface(face);
+            if(extrema.Perform(curve))
+            {
+                int nCount = extrema.GetPointCount();
+                MessageBox.Show(nCount.ToString());
+            }
+
+            renderer.ShowShape(edge, Vector3.Blue);
+            renderer.ShowShape(face, Vector3.LightGray);
+        }
+    }
+}

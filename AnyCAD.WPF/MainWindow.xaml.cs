38bc22b35225762a0648cff8d43c50aaa741f9b6
anycad <anycad@126.com>
2020-11-25 12:32:39 +0800

    add WPF

---
+++ AnyCAD.WPF/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using AnyCAD.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AnyCAD.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        RenderControl mRenderCtrl;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            mRenderCtrl = new RenderControl();
+            this.viewHost.Child = mRenderCtrl;
+        }
+    }
+}

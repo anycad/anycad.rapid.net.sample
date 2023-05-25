47e5e9567b0f481a5ee011523d40ddffe5b46b51
cloud <cloud@qq.com>
2023-05-25 14:39:03 +0800

    高级示例：交互编辑对象

--- AnyCAD.Advanced/Interaction/Interaction_Measure.cs
+++ AnyCAD.Advanced/Interaction/Interaction_Measure.cs
@@ -1,45 +1,34 @@
-﻿using AnyCAD.Foundation;
+﻿using AnyCAD.Demo.Geometry;
+using AnyCAD.Foundation;
 using System;
 
 namespace AnyCAD.Demo.Graphics
 {
-    //class MyEditorListner : EditorListener
-    //{
-    //    public override void OnChanging(string name, ViewContext ctx, SceneNode pNode, ObjectIdSet ids)
-    //    {
-
-    //    }
-    //    public override void OnChanged(string name, ViewContext ctx, SceneNode pNode, ObjectIdSet ids)
-    //    {
-    //        if (name == "MeasureTwoPoints")
-    //        {
-    //            var mdn = MeasureDistanceNode.Cast(pNode);
-    //            if(mdn != null)
-    //            {
-    //                var start = mdn.GetStart().GetPosition();
-    //                var end = mdn.GetEnd().GetPosition();
-    //                DialogUtil.ShowMessageBox("Info", String.Format("Measure Info: Distance: {6} \n{0},{1},{2} --> {3},{4},{5}", 
-    //                    start.x, start.y, start.z, end.x, end.y, end.z, start.distanceTo(end)));
-    //            }
-               
-    //        }
-    //    }
-    //}
-
-    class Interaction_Measure : Graphics_PointCloud
+    class MyDistanceMeasureEditor : DistanceMeasureEditor
     {
-        //MyEditorListner mEditorListener;
+        public MyDistanceMeasureEditor()
+        {
+            //SetRepeat(true);
+        }
+        public override void Apply(ViewContext ctx)
+        {
+            var node = GetNode();
+            base.Apply(ctx);
+        }
+    }
+
+    class Interaction_Measure : Analysis_Holes
+    {
         public override void Run(IRenderView render)
         {
             base.Run(render);
 
-            //if(mEditorListener == null)
-            //{
-            //    mEditorListener = new MyEditorListner();
-            //    EditorEvent.Instance().AddListener(mEditorListener);
-            //}
+            render.SetEditor(new MyDistanceMeasureEditor());
+        }
 
-            //AnyCAD.Foundation.Application.Instance().ExecuteCommand("MeasureTwoPoints");
+        public override void Exit(IRenderView render)
+        {
+            render.SetEditor(null);
         }
     }
 }
---
+++ AnyCAD.Advanced/Interaction/Interaction_MoveAndRotate.cs
@@ -0,0 +1,24 @@
+﻿using AnyCAD.Demo.Geometry;
+using AnyCAD.Foundation;
+using System;
+
+namespace AnyCAD.Demo.Graphics
+{
+    class Interaction_MoveAndRotate : TestCase
+    {
+        public override void Run(IRenderView render)
+        {
+            var shape = GeometryBuilder.CreateBox(10, 100, 200);
+            var node = PrimitiveSceneNode.Create(shape, null);
+            render.ShowSceneNode(node);
+
+            var editor = new NodeFrameEidtor(node, TransformWidget.Create(20), "MyNodeEditor");
+            render.SetEditor(editor);
+        }
+
+        public override void Exit(IRenderView render)
+        {
+            render.SetEditor(null);
+        }
+    }
+}

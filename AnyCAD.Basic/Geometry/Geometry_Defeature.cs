4c30879ead6219f440dfced1b96225a814e03d7e
cloud <cloud@qq.com>
2023-07-15 09:49:23 +0800

    Update SDK

---
+++ AnyCAD.Basic/Geometry/Geometry_Defeature.cs
@@ -0,0 +1,26 @@
+﻿using AnyCAD.Foundation;
+
+
+namespace AnyCAD.Demo.Geometry
+{
+    class Geometry_Defeature : TestCase
+    {
+        public override void Run(IRenderView render)
+        {
+            string fileName = GetResourcePath("feature/底封板.stp");
+            var shape = StepIO.Open(fileName);
+            if (shape == null)
+                return;
+
+            var fT = shape.GetShapeType();
+            GlobalInstance.Log(fT.ToString());
+            var faces = shape.GetChildren(EnumTopoShapeType.Topo_FACE);
+            foreach( var face in faces )
+            {
+                var newShape = FixShapeTool.RemoveInternalWires(face, 100000);
+                render.ShowShape(newShape, ColorTable.LightBLue);
+            }
+
+        }
+    }
+}
---
+++ AnyCAD.Basic/Geometry/Geometry_Sew.cs
@@ -0,0 +1,20 @@
+﻿using AnyCAD.Foundation;
+using System;
+using System.IO;
+using System.Text;
+
+namespace AnyCAD.Demo.Geometry
+{
+    class Geometry_Sew : TestCase
+    {
+        public override void Run(IRenderView render)
+        {
+            var face1 = ShapeIO.Open(GetResourcePath("boolean/face1.igs"));
+            var face2 = ShapeIO.Open(GetResourcePath("boolean/face2.igs"));
+
+            var face = BooleanTool.Fuse(face1, face2);
+
+            render.ShowShape(face, ColorTable.LightBLue);
+        }
+    }
+}

c3d0672a83a175388650da026b0ed8832b396c78
cloud <cloud@qq.com>
2023-08-04 10:48:45 +0800

    透明物体绘制顺序

--- AnyCAD.Basic/Graphics/Graphics_RenderOrder.cs
+++ AnyCAD.Basic/Graphics/Graphics_RenderOrder.cs
@@ -23,11 +23,11 @@ namespace AnyCAD.Demo.Graphics
             materialBig.SetColor(ColorTable.SkyBlue);
 
             var bigBox = GeometryBuilder.CreateBox(100, 100, 100);
-            //materialBig.SetRenderOrder(bigBox.GetSafeBoudingBox().volume());
+            materialBig.SetRenderOrder(bigBox.GetSafeBoudingBox().volume());
             var node = PrimitiveSceneNode.Create(bigBox, materialBig);
 
             var smallBox = GeometryBuilder.CreateBox(50, 50, 90);
-            //materialSmall.SetRenderOrder(smallBox.GetSafeBoudingBox().volume());
+            materialSmall.SetRenderOrder(smallBox.GetSafeBoudingBox().volume());
             var node2 = PrimitiveSceneNode.Create(smallBox, materialSmall);
 
             var core = GeometryBuilder.CreateSphere(10, 32, 32);

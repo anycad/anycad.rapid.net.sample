f3648e2da392695efc48e58f82ef82c8d19936c8
malw <anycad@qq.com>
2024-04-01 11:53:11 +0800

    AdvCurveBuilder.MakeText

---
+++ AnyCAD.Advanced/Geometry/Geometry_TextOutline.cs
@@ -0,0 +1,33 @@
+﻿using AnyCAD.Foundation;
+
+namespace AnyCAD.Demo.Geometry
+{
+    class Geometry_TextOutline : TestCase
+    {
+        public override void Run(IRenderView render)
+        {
+            var wires = AdvCurveBuilder.MakeText("招财进宝", "");
+
+            var edges = wires.GetChildren(EnumTopoShapeType.Topo_EDGE);
+            for(var i = 0; i < edges.Count; i++)
+            {
+                var edge = edges[i];
+
+                var curve = new ParametricCurve(edge);
+                var ps = curve.SplitByUniformLength(20, GP.Resolution());
+                foreach(var p in ps)
+                {
+                    var d1 = curve.D1(p);
+                    var dir = d1.GetVectors()[0];
+                    var trf = new GAx2(d1.GetPoint(), new GDir(0, 0, 1), new GDir(dir));
+                    var ax = AxWidget.Create(new Vector3(20), 1);
+                    ax.SetTransform(Matrix4.From(trf.ToTrsf()));
+
+                    render.ShowSceneNode(ax);
+                }
+            }
+
+            render.ShowShape(wires, ColorTable.玫瑰棕色);
+        }
+    }
+}
---
+++ AnyCAD.Advanced/Geometry/Geometry_Tube.cs
@@ -0,0 +1,38 @@
+﻿using AnyCAD.Foundation;
+
+namespace AnyCAD.Demo.Geometry
+{
+    class Geometry_Tube : TestCase
+    {
+        public override void Run(IRenderView render)
+        {
+            // 圆管
+            var tube = AdvShapeBuilder.MakeTube(new GAx2(), 8, 1, 100);
+
+            // 矩形管
+            var rectTube = AdvShapeBuilder.MakeRectTube(new GAx2(new GPnt(40, 0, 0), new GDir(0, 0, 1)), 15, 20, 2, 2, 100);
+
+            // 实心
+            var rectSolid = AdvShapeBuilder.MakeRectSolid(new GAx2(new GPnt(40, 40, 0), new GDir(0, 0, 1)), 15, 20, 2, 100);
+
+
+            // 腰管
+            var waistTube = AdvShapeBuilder.MakeWaistTube(new GAx2(new GPnt(80, 0, 0), new GDir(0, 0, 1)), 20, 5, 1, 100);
+
+            var waistSolid = AdvShapeBuilder.MakeWaistSolid(new GAx2(new GPnt(80, 40, 0), new GDir(0, 0, 1)), 20, 5, 100);
+
+
+            var edgeMaterial = LineDashedMaterial.Create("tube.material.edge");
+            edgeMaterial.SetColor(ColorTable.Wheat);
+
+            // 显示
+            render.ShowSceneNode(BrepSceneNode.Create(tube, MaterialStore.Chrome, edgeMaterial));
+            
+            render.ShowSceneNode(BrepSceneNode.Create(rectTube, MaterialStore.Copper, edgeMaterial));
+            render.ShowSceneNode(BrepSceneNode.Create(rectSolid, MaterialStore.Brass, edgeMaterial));
+
+            render.ShowSceneNode(BrepSceneNode.Create(waistTube, MaterialStore.YellowPlastic, edgeMaterial));
+            render.ShowSceneNode(BrepSceneNode.Create(waistSolid, MaterialStore.YellowRubber, edgeMaterial));
+        }
+    }
+}
